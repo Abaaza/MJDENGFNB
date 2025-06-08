c8b7fe3494594e7028b13d1ddd3a15f00d799893
Ahmed Abaza <a.abaza90@gmail.com>
2025-06-08 17:23:39 +0300

    20

--- backend/src/handler.js
+++ backend/src/handler.js
@@ -1,6 +1,14 @@
-//src/handler.js
-
 import serverless from 'serverless-http';
 import app from './app.js';
 
-export const handler = serverless(app);
+export const handler = serverless(app, {
+  request: {
+    rawBody: false, // default is false, keep this
+    body: 'body',
+    headers: 'headers',
+    method: 'httpMethod',
+    path: 'path',
+    requestContext: 'requestContext',
+    query: 'queryStringParameters'
+  }
+});
