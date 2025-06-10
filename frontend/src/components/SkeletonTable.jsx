dc5bf607f3b7ce59420ecd391f115ad8a441e622
Ahmed Abaza <a.abaza90@gmail.com>
2025-06-10 03:48:32 +0300

    15

--- frontend/src/components/SkeletonTable.jsx
+++
@@ -1,9 +0,0 @@
-export default function SkeletonTable() {
-  return (
-    <div className="animate-pulse space-y-2">
-      {Array.from({ length: 6 }).map((_, i) => (
-        <div key={i} className="h-8 bg-gray-200 rounded" />
-      ))}
-    </div>
-  );
-}
